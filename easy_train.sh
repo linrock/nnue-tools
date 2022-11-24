4a3992a6c3bea8c1e450ed7a930948def1ccac7b
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-23 21:06:27 -0500

    Add example easy_train.sh script locally

---
+++ easy_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+python3 easy_train.py \
+  --training-dataset=/app/nodes5000pv2_UHO.binpack \
+  --validation-dataset=/app/nodes5000pv2_UHO.binpack \
+  --gpus="0," \
+  --experiment-name=2 \
+  --additional-training-arg=--param-index=2 \
+  --start-lambda=1.0 \
+  --end-lambda=0.75 \
+  --gamma=0.995 \
+  --lr=4.375e-4 \
+  --start-from-engine-test-net True \
+  --tui=False \
+  --seed=$RANDOM \
+  --max_epoch=800 \
+  --auto-exit-timeout-on-training-finished=900 \
+  --network-testing-threads 8  \
+  --num-workers 48
+  # --engine-base-branch=official-stockfish/Stockfish/master \
+  # --engine-test-branch=official-stockfish/Stockfish/master
