3c9fa1720edee1f46c5c656f487b34b3cd2c0d09
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-24 18:48:55 -0500

    Start detached containers and attach with bash

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-docker run -it \
+# Run the container in the background (detached)
+docker_container_id=$(docker run -d \
   --gpus all \
   --mount type=bind,source="$(pwd)"/data,target=/root/data \
   --mount type=bind,source="$(pwd)"/easy_train_data,target=/root/nnue-pytorch/scripts/easy_train_data \
-  nnue-pytorch bash
+  nnue-pytorch)
+
+# Attach a bash shell to the running container
+docker exec -it $docker_container_id bash
