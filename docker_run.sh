8b720c6363c6874effaae239b62998120939da6c
Linmiao Xu <linmiao.xu@gmail.com>
2022-11-27 02:41:46 -0500

    Bind mount in root dir, symlink from scripts dir

--- docker_run.sh
+++ docker_run.sh
@@ -4,12 +4,16 @@
 docker_container_id=$(docker run -d \
   --gpus all \
   --mount type=bind,source="$(pwd)"/data,target=/root/data \
-  --mount type=bind,source="$(pwd)"/easy_train_data,target=/root/nnue-pytorch/scripts/easy_train_data \
+  --mount type=bind,source="$(pwd)"/easy_train_data,target=/root/easy_train_data \
   nnue-pytorch)
 
 # Copy a usable easy_train.sh script in at runtime
 docker cp easy_train.sh $docker_container_id:/root/
 docker exec -it $docker_container_id chmod +x /root/easy_train.sh
+
+# Symlink the data directory to the bind mounted easy_train_data
+docker exec -it $docker_container_id \
+  ln -s /root/easy_train_data /root/nnue-pytorch/scripts/
 
 # Attach a bash shell to the running container
 docker exec -it $docker_container_id bash
