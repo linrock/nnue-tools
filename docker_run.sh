feb428aa4ecb2828771e8db81a80ea16f35014cc
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-01 14:34:19 +0000

    Renaming data directories for convenience

--- docker_run.sh
+++ docker_run.sh
@@ -3,8 +3,8 @@
 # Run the container in the background (detached)
 docker_container_id=$(docker run -d \
   --gpus all \
-  --mount type=bind,source="$(pwd)"/data,target=/root/data \
-  --mount type=bind,source="$(pwd)"/easy_train_data,target=/root/easy_train_data \
+  --mount type=bind,source="$(pwd)"/training-data,target=/root/training-data \
+  --mount type=bind,source="$(pwd)"/easy-train-data,target=/root/easy-train-data \
   nnue-pytorch)
 
 # Copy a usable run_easy_train.sh script in at runtime
@@ -13,7 +13,7 @@ docker exec -it $docker_container_id chmod +x /root/run_easy_train.sh
 
 # Symlink the data directory to the bind mounted easy_train_data
 docker exec -it $docker_container_id \
-  ln -s /root/easy_train_data /root/nnue-pytorch/scripts/
+  ln -s /root/easy-train-data /root/nnue-pytorch/scripts/easy_train_data
 
 # Attach a bash shell to the running container
 docker exec -it $docker_container_id bash
