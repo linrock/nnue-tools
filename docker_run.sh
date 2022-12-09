9d193b38733e2433dc3b4ba4792e21f81d10def5
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-09 17:20:24 -0500

    Ensure checkpoint train script is runnable

--- docker_run.sh
+++ docker_run.sh
@@ -9,9 +9,11 @@ docker_container_id=$(docker run -d \
   nnue-pytorch)
 
 # Copy a usable run_easy_train.sh script in at runtime
+docker cp checkpoint_train.sh $docker_container_id:/root/
 docker cp master_net_train.sh $docker_container_id:/root/
 docker cp new_net_train.sh $docker_container_id:/root/
 docker cp print_rankings.sh $docker_container_id:/root/
+docker exec -it $docker_container_id chmod +x /root/checkpoint_train.sh
 docker exec -it $docker_container_id chmod +x /root/master_net_train.sh
 docker exec -it $docker_container_id chmod +x /root/new_net_train.sh
 docker exec -it $docker_container_id chmod +x /root/print_rankings.sh
