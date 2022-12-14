4f6eedb12db2876152ff60287999b76af7ab849d
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-14 15:04:51 -0500

    Allow filtering experiments to print rankings of

--- print_rankings.sh
+++ print_rankings.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+search_filter=$1
 for ordo in $(ls -1 easy-train-data/experiments/*/training/ordo.out); do
-  echo $ordo
-  cat $ordo
+  if [ -z $search_filter ]; then
+    echo $ordo
+    cat $ordo
+  else
+    if ls $ordo | grep ${search_filter}; then
+      cat $ordo
+    fi
+  fi
 done
