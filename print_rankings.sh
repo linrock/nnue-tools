f0cd9f186b38aae85eaad3ea6ba01f972f0d0eb7
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-15 23:15:58 -0500

    Print last line of log if filtering rankings

--- print_rankings.sh
+++ print_rankings.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 search_filter=$1
-for ordo in $(ls -1 easy-train-data/experiments/*/training/ordo.out); do
+for ordo_file in $(ls -1 easy-train-data/experiments/*/training/ordo.out); do
   if [ -z $search_filter ]; then
-    echo $ordo
-    cat $ordo
+    echo $ordo_file
+    cat $ordo_file
   else
-    if ls $ordo | grep ${search_filter}; then
-      cat $ordo
+    if ls $ordo_file | grep ${search_filter}; then
+      cat $ordo_file
+      tail -1 $(dirname $ordo_file)/../logging/easy_train.log
     fi
   fi
 done
