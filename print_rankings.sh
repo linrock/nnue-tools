1b30e639a72d41353ee365d69d3a6ffc712419de
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-26 02:44:38 -0500

    Fix printing current epoch when printing rankings

--- print_rankings.sh
+++ print_rankings.sh
@@ -8,7 +8,8 @@ for ordo_file in $(ls -1 easy-train-data/experiments/*/training/ordo.out); do
   else
     if ls $ordo_file | grep ${search_filter}; then
       cat $ordo_file
-      tail -20 $(dirname $ordo)/../logging/easy_train.log | grep Epoch | tail -1
+      tail -50 $(dirname $ordo_file)/../logging/easy_train.log | grep Epoch | tail -1
+      echo
     fi
   fi
 done
