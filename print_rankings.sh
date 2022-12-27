45866b6273752a69bffe942ee629d4d47035b593
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-27 15:00:57 -0500

    Don't print full path to ordo file

--- print_rankings.sh
+++ print_rankings.sh
@@ -6,7 +6,7 @@ for ordo_file in $(ls -1 easy-train-data/experiments/*/training/ordo.out); do
     echo $ordo_file
     cat $ordo_file
   else
-    if ls $ordo_file | grep ${search_filter}; then
+    if ls $ordo_file | grep ${search_filter} > /dev/null; then
       echo $(echo $ordo_file | grep -oE experiment_.*)
       tail -100 $(dirname $ordo_file)/../logging/easy_train.log | grep Epoch | tail -1
       cat $ordo_file
