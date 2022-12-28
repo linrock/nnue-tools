03759b5fb5f53561c6394bb460db4264b5f33d0b
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-28 14:58:22 -0500

    Set hash size as part of tc options

--- run_gauntlet.sh
+++ run_gauntlet.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# tc_options="tc=10000+10000 nodes=25000"
-tc_options="tc=10+0.1"
+# tc_options="option.Hash=8 tc=10000+10000 nodes=25000"
+tc_options="option.Hash=16 tc=10+0.1"
+
 num_games=100000
 concurrency=16
 
 experiment=experiment_leela-dfrc-filtered-T80-sep-oct-nov
 nn_to_test=run_0/nn-epoch779.nnue
-
-pgn_out=/root/test_gauntlet_sepoctnov_759_stc.pgn
+pgn_out=/root/test_gauntlet_sepoctnov_779_stc.pgn
 
 /root/easy-train-data/c-chess-cli/c-chess-cli -gauntlet \
   -games $num_games \
   -rounds 1 -concurrency $concurrency \
-  -each option.Hash=16 option.Threads=1 timeout=20 $tc_options \
+  -each option.Threads=1 timeout=20 $tc_options \
   -openings \
     file=/root/easy-train-data/books/UHO_XXL_+0.90_+1.19.epd \
     order=random srand=${RANDOM}${RANDOM} \
