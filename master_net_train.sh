7bdeeccd76a6cbc7aec4366a327e06f25b9f8b3a
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-08 15:34:07 -0500

    Update defaults for master net train

--- master_net_train.sh
+++ master_net_train.sh
@@ -3,26 +3,25 @@
 
 experiment_name=Leela-dfrc_n5000
 training_dataset=/root/training-data/Leela-dfrc_n5000.binpack
+nnue_pytorch_branch=linrock/nnue-pytorch/misc-fixes
+gpus="0,"
 
 cd nnue-pytorch/scripts
 python3 easy_train.py \
-  --experiment-name=$experiment_name \
-  --training-dataset=$training_dataset \
+  --experiment-name $experiment_name \
+  --training-dataset $training_dataset \
+  --nnue-pytorch-branch $nnue_pytorch_branch \
+  --gpus $gpus \
+  --seed $RANDOM \
   --start-from-engine-test-net True \
-  --gpus="0," \
-  --start-lambda=1.0 \
-  --end-lambda=0.75 \
-  --gamma=0.995 \
-  --lr=4.375e-4 \
-  --tui=False \
-  --seed=$RANDOM \
-  --max_epoch=800 \
-  --nnue-pytorch-branch=linrock/nnue-pytorch/misc-fixes \
-  --workspace-path=/root/easy-train-data \
-  --build-engine-arch=x86-64-bmi2 \
+  --start-lambda 1.0 \
+  --end-lambda 0.75 \
+  --gamma 0.995 \
+  --lr 4.375e-4 \
+  --tui False \
+  --max_epoch 800 \
+  --workspace-path /root/easy-train-data \
+  --build-engine-arch x86-64-bmi2 \
   --network-testing-games-per-round 1000 \
-  --network-testing-threads 8 \
-  --num-workers 12
-
-# --network-testing-hash-mb=16 \
-# --auto-exit-timeout-on-training-finished=900 \
+  --network-testing-threads 24 \
+  --num-workers 6
