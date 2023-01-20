5f54795e54f21115b4596b9be124cf597c296fd1
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-20 14:25:57 -0500

    Take target sha256 prefix as an input

--- misc/edit_random_bytes.py
+++ misc/edit_random_bytes.py
@@ -2,6 +2,10 @@ import hashlib
 import secrets
 import sys
 
+if len(sys.argv) != 3:
+    print('Usage: ./edit_random_bytes.py <nnue_filename> <sha256_prefix>')
+    sys.exit(0)
+
 def get_sha256_hash(nnue_filename):
     with open(nnue_filename, "rb") as f:
         return hashlib.sha256(f.read()).hexdigest()
@@ -13,18 +17,17 @@ def random_non_functional_edit(nnue_filename):
         f.seek(79) # trainer
         f.write(secrets.token_bytes(7))
 
-if len(sys.argv) != 2:
-    print('Usage: ./test.py <nnue_filename>')
-    sys.exit(0)
 nnue_filename = sys.argv[1]
+sha256_prefix = sys.argv[2]
 num_tries = 0
 sha256 = ''
-print(f'Modifying {nnue_filename}')
-while not sha256.startswith('1337'):
+
+print(f'Modifying {nnue_filename} to have sha256 prefix: {sha256_prefix}')
+while not sha256.startswith(sha256_prefix):
     random_non_functional_edit(nnue_filename)
     sha256 = get_sha256_hash(nnue_filename)
     num_tries += 1
     if num_tries % 100 == 0:
         print(f'Tried {num_tries} times')
 
-print(f'{sha256} after {num_tries}')
+print(f'{sha256} after {num_tries} tries')
