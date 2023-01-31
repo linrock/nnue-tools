2dff884c967dbbe02c1453433b7e121b0c748664
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-31 10:59:19 -0500

    List experiments, enable auto-reload

--- api/server.py
+++ api/server.py
@@ -6,10 +6,19 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', response_class=HTMLResponse)
 def list_experiments():
-    return glob('easy-train-data/experiments/*')
+    experiments_list = glob('easy-train-data/experiments/*')
+    return f'''
+    <html>
+        <body>
+            <ul>
+                {''.join([f'<li>{experiment}</li>' for experiment in experiments_list])}
+            </ul>
+        </body>
+    </html>
+    '''
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=58000)
+    uvicorn.run('api:app', host="127.0.0.1", port=58000, reload=True)
