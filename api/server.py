e8503f0f3cd02eb4c62e5ae3eeb08422b7c78a9d
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-31 11:07:17 -0500

    Clickable links to experiment pages

--- api/server.py
+++ api/server.py
@@ -9,12 +9,28 @@ app = FastAPI()
 @app.get('/', response_class=HTMLResponse)
 def list_experiments():
     experiments_list = glob('easy-train-data/experiments/*')
+    experiment_links = []
+    for exp in experiments_list:
+        exp_name = exp.split('/')[-1]
+        experiment_links.append(f'<li><a href="{exp_name}">{exp_name}</a></li>')
     return f'''
     <html>
         <body>
+            <h3>Experiments</h3>
             <ul>
-                {''.join([f'<li>{experiment}</li>' for experiment in experiments_list])}
+                {''.join(experiment_links)}
             </ul>
+        </body>
+    </html>
+    '''
+
+@app.get('/{exp_name}', response_class=HTMLResponse)
+def view_experiment(exp_name: str):
+    return f'''
+    <html>
+        <body>
+            <h3>Experiment</h3>
+            <h4>{exp_name}</h4>
         </body>
     </html>
     '''
