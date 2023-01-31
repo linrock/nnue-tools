5dbfc7aac2e048186dd21e06f551e44bc137447c
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-31 11:30:50 -0500

    Sort experiments by how recently games were played

--- api/server.py
+++ api/server.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from glob import glob
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 import uvicorn
@@ -9,16 +12,27 @@ app = FastAPI()
 @app.get('/', response_class=HTMLResponse)
 def list_experiments():
     experiments_list = glob('easy-train-data/experiments/*')
-    experiment_links = []
+    experiments = []
+    experiment_rows = []
     for exp in experiments_list:
         exp_name = exp.split('/')[-1]
-        experiment_links.append(f'<li><a href="{exp_name}">{exp_name}</a></li>')
+        exp_last_modified = os.path.getmtime(f'easy-train-data/experiments/{exp_name}/training/out.pgn')
+        experiments.append({
+            'name': exp_name,
+            'last_updated': exp_last_modified,
+            'last_updated_str': datetime.fromtimestamp(exp_last_modified).strftime("%b %-d")
+        })
+    experiments = sorted(experiments, key=lambda exp: -exp['last_updated'])
+    for exp in experiments:
+        experiment_rows.append(f'''
+            <li><a href="{exp['name']}">{exp['name']}</a> {exp['last_updated_str']}</li>
+        ''')
     return f'''
     <html>
         <body>
             <h3>Experiments</h3>
             <ul>
-                {''.join(experiment_links)}
+                {''.join(experiment_rows)}
             </ul>
         </body>
     </html>
