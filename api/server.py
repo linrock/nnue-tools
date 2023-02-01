1cbbf3475c2667d8478191e08283a452b77a7fb0
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-01 04:49:12 +0000

    Use https for api server by default

--- api/server.py
+++ api/server.py
@@ -114,4 +114,6 @@ def view_experiment(exp_name: str, api_key: str = ''):
 
 
 if __name__ == "__main__":
-    uvicorn.run('server:app', host="127.0.0.1", port=58000, reload=True)
+    uvicorn.run('server:app', host="127.0.0.1", port=58000, reload=True,
+                ssl_keyfile='./key.pem',
+                ssl_certfile='./cert.pem')
