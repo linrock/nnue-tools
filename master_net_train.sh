d7044fefd255211e2893f815be21fa0bbc925dd6
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-20 14:36:05 -0500

    Set gpu string based on filename during training

--- master_net_train.sh
+++ master_net_train.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 # Retrain the current master .nnue on the given training dataset
-
+source ./misc/utils.sh
 experiment_name=leela93-dfrc99-filt-only-T80octnov
 training_dataset=/root/training-data/leela93-dfrc99-filt-only-T80octnov.binpack
 nnue_pytorch_branch=linrock/nnue-pytorch/skip28
-gpus="0,"
 
 cd nnue-pytorch/scripts
 python3 easy_train.py \
-  --experiment-name $experiment_name \
-  --training-dataset $training_dataset \
-  --nnue-pytorch-branch $nnue_pytorch_branch \
-  --gpus $gpus \
-  --seed $RANDOM \
   --start-from-engine-test-net True \
   --start-lambda 1.0 \
   --end-lambda 0.75 \
-  --gamma 0.995 \
-  --lr 4.375e-4 \
-  --tui False \
   --max_epoch 800 \
-  --workspace-path /root/easy-train-data \
-  --build-engine-arch x86-64-bmi2 \
   --network-testing-games-per-round 1000 \
   --network-testing-threads 24 \
-  --num-workers 6
+  --workspace-path /root/easy-train-data \
+  --build-engine-arch x86-64-bmi2 \
+  --num-workers 6 \
+  --lr 4.375e-4 \
+  --gamma 0.995 \
+  --tui False \
+  --seed $RANDOM \
+  --gpus $(filename_to_gpu_str) \
+  --experiment-name $experiment_name \
+  --training-dataset $training_dataset \
+  --nnue-pytorch-branch $nnue_pytorch_branch
