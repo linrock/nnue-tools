c3265b95d333b36ad32d78cf66a3de3cc985de87
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-20 14:42:56 -0500

    Rearrange master net train args

--- master_net_train.sh
+++ master_net_train.sh
@@ -3,21 +3,22 @@
 source ./misc/utils.sh
 experiment_name=leela93-dfrc99-filt-only-T80octnov
 training_dataset=/root/training-data/leela93-dfrc99-filt-only-T80octnov.binpack
-nnue_pytorch_branch=linrock/nnue-pytorch/skip28
+nnue_pytorch_branch=linrock/nnue-pytorch/misc-fixes
 
 cd nnue-pytorch/scripts
 python3 easy_train.py \
-  --start-from-engine-test-net True \
+  --early-fen-skipping 28 \
+  --max_epoch 800 \
   --start-lambda 1.0 \
   --end-lambda 0.75 \
-  --max_epoch 800 \
   --network-testing-games-per-round 1000 \
   --network-testing-threads 24 \
-  --workspace-path /root/easy-train-data \
-  --build-engine-arch x86-64-bmi2 \
   --num-workers 6 \
+  --start-from-engine-test-net True \
   --lr 4.375e-4 \
   --gamma 0.995 \
+  --build-engine-arch x86-64-bmi2 \
+  --workspace-path /root/easy-train-data \
   --tui False \
   --seed $RANDOM \
   --gpus $(filename_to_gpu_str) \
