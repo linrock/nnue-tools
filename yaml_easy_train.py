d83b4c203405be9993917dbe195c3dbfa7200d9c
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-02 22:36:47 -0400

    Set seed and tui by default

--- yaml_easy_train.py
+++ yaml_easy_train.py
@@ -1,8 +1,15 @@
+import random
 import yaml
+
+default_args = {
+    'seed': random.randint(9999, 9_999_999),
+    'tui': False,
+}
 
 with open("config.yml", "r") as stream:
     try:
         args = yaml.safe_load(stream)
+        args = {**default_args, **args}
         command = ["python3 easy_train.py"]
         for key,value in sorted(args.items()):
             command.append(f"  --{key} {value}")
