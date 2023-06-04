b5fc25b20684cfc76ba6d30c053696775573a953
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-04 14:32:32 -0400

    Use gpus defined in filename

--- yaml_easy_train.py
+++ yaml_easy_train.py
@@ -1,4 +1,5 @@
 import random
+import re
 import sys
 import yaml
 
@@ -7,13 +8,23 @@ default_args = {
     'tui': False,
 }
 
-with open(sys.argv[1], "r") as stream:
+yaml_config_file = sys.argv[1]
+with open(yaml_config_file, "r") as stream:
     try:
         args = yaml.safe_load(stream)
         args = {**default_args, **args}
+
+        # if config filename contains gpu ids, automatically use them
+        gpus_from_filename = re.search(r"gpu(\d+)", yaml_config_file)
+        if gpus_from_filename:
+            gpus_str = ",".join(list(gpus_from_filename.group(1)))
+            args["gpus"] = gpus_str
+
+        # prepare an easy_train.py command for training
         command = ["python3 easy_train.py"]
         for key,value in sorted(args.items()):
             command.append(f"  --{key} {value}")
+
         if len(sys.argv) > 2 and sys.argv[2] == "print":
             print(" \\\n".join(command))
         else:
