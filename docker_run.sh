6bc61051ab2aac31eba039a2b14f5261475af636
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-12 00:03:01 -0400

    Don't need to copy in at runtime

--- docker_run.sh
+++ docker_run.sh
@@ -11,10 +11,7 @@ docker_container_id=$(docker run -d \
 
 # Copy a usable run_easy_train.sh script in at runtime
 docker cp print_rankings.sh $docker_container_id:/root/
-docker cp train.sh $docker_container_id:/root/
-
 docker exec -it $docker_container_id chmod +x /root/print_rankings.sh
-docker exec -it $docker_container_id chmod +x /root/train.sh
 
 # Attach a bash shell to the running container
 docker exec -it $docker_container_id bash
